689484e58eafe47f2e210672045a97efed1bfa73
Muslum Şahin <sahinmuslumm@gmail.com>
2023-07-06 12:59:26 +0300

    Add files via upload
    Patika-PHP while ve for döngüsü ödevi hazırlandı.

---
+++ ucgen.php
@@ -0,0 +1,28 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-UCGEN</title>
+</head>
+<body>
+
+
+<?php
+$a = 1;
+while($a < 10){
+    for($i = 0; $i<$a; $i++){
+        echo '0';
+    }
+    $a++;
+    echo "<br>";
+}
+
+
+?>
+
+
+
+</body>
+</html>
+
